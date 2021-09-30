6e21a0b80635345b34e32dd89b2a5d452e8ac1e0
Rajan Agarwal <rajan.ag005@gmail.com>
2021-09-30 17:29:42 -0400

    this should actually work

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -1,306 +1,45 @@
-import {
-  Box,
-  Center,
-  useColorModeValue,
-  Heading,
-  Text,
-  Stack,
-  Image,
-} from '@chakra-ui/react';
+import React from "react";
+import Card1 from './Card1';
 
-const IMAGE =
-  'https://cloud-ghn6xcuv0-hack-club-bot.vercel.app/0hero__2_.png';
+// reactstrap components
+import { Container, Row, Col } from "react-grid-system";
 
-export default function Card() {
+function Example() {
   return (
-    <Center py={20}>
-      <Box
-        role={'group'}
-        p={6}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `https://cloud-ghn6xcuv0-hack-club-bot.vercel.app/0hero__2_.png`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={282}
-            objectFit={'cover'}
-            src={IMAGE}
-            alt={'decaphoto'}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            President
-          </Text>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            Tanya Puri
-          </Heading>
-          <Stack direction={'row'} align={'center'}>
-            <Text fontWeight={400} fontSize={'xl'}>
-              <i>Synergy</i>
-            </Text>
-          </Stack>
-        </Stack>
-      </Box>
+    <>
+      <Container>
+        <Row>
+          <Col sm={3}>
+            <Card1/>
+          </Col>
+          <Col sm={3}>
+          <Card1/>
+          </Col>
+          <Col sm={3}>
+          <Card1/>
+          </Col>
+          <Col sm={3}>
+          <Card1/>
+          </Col>
+        </Row>
 
-
-
-      <Box
-        role={'group'}
-        p={6}
-        left={12}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `https://cloud-ghn6xcuv0-hack-club-bot.vercel.app/0hero__2_.png`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={282}
-            objectFit={'cover'}
-            src={IMAGE}
-            alt={'decaphoto'}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            President
-          </Text>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            Tanya Puri
-          </Heading>
-          <Stack direction={'row'} align={'center'}>
-            <Text fontWeight={400} fontSize={'xl'}>
-              <i>Synergy</i>
-            </Text>
-          </Stack>
-        </Stack>
-      </Box>
-
-<Box
-        role={'group'}
-        p={6}
-        left={24}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `https://cloud-ghn6xcuv0-hack-club-bot.vercel.app/0hero__2_.png`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={282}
-            objectFit={'cover'}
-            src={IMAGE}
-            alt={'decaphoto'}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            President
-          </Text>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            Tanya Puri
-          </Heading>
-          <Stack direction={'row'} align={'center'}>
-            <Text fontWeight={400} fontSize={'xl'}>
-              <i>Synergy</i>
-            </Text>
-          </Stack>
-        </Stack>
-      </Box>
-
-
-
-      <Box
-        role={'group'}
-        p={6}
-        left={36}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `https://cloud-ghn6xcuv0-hack-club-bot.vercel.app/0hero__2_.png`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={282}
-            objectFit={'cover'}
-            src={IMAGE}
-            alt={'decaphoto'}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            President
-          </Text>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            Tanya Puri
-          </Heading>
-          <Stack direction={'row'} align={'center'}>
-            <Text fontWeight={400} fontSize={'xl'}>
-              <i>Synergy</i>
-            </Text>
-          </Stack>
-        </Stack>
-      </Box>
-
-      <Box
-        role={'group'}
-        p={6}
-        left={48}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `https://cloud-ghn6xcuv0-hack-club-bot.vercel.app/0hero__2_.png`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={282}
-            objectFit={'cover'}
-            src={IMAGE}
-            alt={'decaphoto'}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            President
-          </Text>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            Tanya Puri
-          </Heading>
-          <Stack direction={'row'} align={'center'}>
-            <Text fontWeight={400} fontSize={'xl'}>
-              <i>Synergy</i>
-            </Text>
-          </Stack>
-        </Stack>
-      </Box>
-    </Center>
+        <Row>
+          <Col sm={3}>
+            <Card1/>
+          </Col>
+          <Col sm={3}>
+          <Card1/>
+          </Col>
+          <Col sm={3}>
+          <Card1/>
+          </Col>
+          <Col sm={3}>
+          <Card1/>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
+
+export default Example;
